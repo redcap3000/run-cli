6ef310d149524f76620adb163ece26c88c48afb9
Ronaldo Barbachano <redcap3000@gmail.com>
2015-12-13 23:03:10 +0000

    updating to use later versions of meteor which support npm, removing old dependency on from npm hacks

--- package.js
+++ package.js
@@ -1,19 +1,19 @@
 Package.describe({
   name: "redcap3000:run-cli",
   summary: "Uses exec() to execute commands serverside.",
-  version: "0.0.1",
+  version: "0.0.2",
   git: "https://github.com/redcap3000/run-cli"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom("0.9.0");
-  api.use(['meteorhacks:npm@1.3.0'], ['server']);
+  api.versionsFrom("1.2.0.2");
+  //api.use(['meteorhacks:npm@1.3.0'], ['server']);
   api.addFiles('runCli.js', ['server']);
   api.export('RunCli',['server']);
 });
 
 Package.onTest(function(api) {
-  api.use(["redcap3000:run-cli",'meteorhacks:npm@1.3.0']);
+  api.use(["redcap3000:run-cli"]);
   api.use('tinytest@1.0.0');
   api.addFiles(['runCli.js','runCli-test.js'], ['server']);
 });
